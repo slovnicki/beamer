e4a924553f608bc4bdab6cb54604967cabaa9515
Sandro Lovnički <lovnicki.sandro@gmail.com>
2021-02-06 22:52:30 +0100

    example/: [update] to be simple books

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -7,32 +7,16 @@ const List<Map<String, String>> books = [
     'id': '1',
     'title': 'Stranger in a Strange Land',
     'author': 'Robert A. Heinlein',
-    'genres': 'Science fiction',
   },
   {
     'id': '2',
     'title': 'Foundation',
     'author': 'Isaac Asimov',
-    'genres': 'Science fiction, Political drama',
   },
   {
     'id': '3',
     'title': 'Fahrenheit 451',
     'author': 'Ray Bradbury',
-    'genres': '	Dystopian',
-  },
-];
-
-const List<Map<String, String>> articles = [
-  {
-    'id': '1',
-    'title': 'Article 1',
-    'author': 'Author 1',
-  },
-  {
-    'id': '2',
-    'title': 'Article 2',
-    'author': 'Author 2',
   },
 ];
 
@@ -45,32 +29,10 @@ class HomeScreen extends StatelessWidget {
         title: Text('Home Screen'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                  onPressed: () =>
-                      context.beamTo(BooksLocation(pathBlueprint: '/books')),
-                  child: Text('Beam to books location'),
-                ),
-                ElevatedButton(
-                  onPressed: () => context.beamTo(BooksLocation(
-                    pathBlueprint: '/books/:bookId',
-                    pathParameters: {'bookId': '2'},
-                  )),
-                  child: Text('Beam to favorite book location'),
-                ),
-              ],
-            ),
-            ElevatedButton(
-              onPressed: () =>
-                  context.beamTo(ArticlesLocation(pathBlueprint: '/articles')),
-              child: Text('Beam to articles location'),
-            ),
-          ],
+        child: ElevatedButton(
+          onPressed: () =>
+              context.beamTo(BooksLocation(pathBlueprint: '/books')),
+          child: Text('Beam to books location'),
         ),
       ),
     );
@@ -120,171 +82,10 @@ class BookDetailsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(book['title']),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            ElevatedButton(
-              onPressed: () => Beamer.of(context).updateCurrentLocation(
-                pathBlueprint: '/books/:bookId/genres',
-                data: {'book': book},
-              ),
-              child: Text('See genres'),
-            ),
-            ElevatedButton(
-              onPressed: () => Beamer.of(context).updateCurrentLocation(
-                pathBlueprint: '/books/:bookId/buy',
-                data: {'book': book},
-              ),
-              child: Text('Buy'),
-            ),
-          ],
-        ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text('Author: ${book['author']}'),
       ),
-    );
-  }
-}
-
-class BuyScreen extends StatelessWidget {
-  BuyScreen({
-    this.book,
-  });
-
-  final Map<String, String> book;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Buy Book'),
-      ),
-      body: Center(
-        child:
-            Text('${book['author']}: ${book['title']}\nbuying in progress...'),
-      ),
-    );
-  }
-}
-
-class GenresScreen extends StatelessWidget {
-  GenresScreen({
-    this.book,
-  }) : genres = book['genres'].split(', ');
-
-  final Map<String, String> book;
-  final List<String> genres;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(book['title'] + "'s genres"),
-      ),
-      body: Center(
-        child: ListView(
-          children: genres
-              .map((genre) => ListTile(
-                    title: Text(genre),
-                    onTap: () => Beamer.of(context).updateCurrentLocation(
-                      pathBlueprint: '/books/:bookId/genres/:genreId',
-                      pathParameters: {
-                        'genreId': (genres.indexOf(genre) + 1).toString()
-                      },
-                      data: {'genre': genre},
-                    ),
-                  ))
-              .toList(),
-        ),
-      ),
-    );
-  }
-}
-
-class GenreDetailsScreen extends StatelessWidget {
-  GenreDetailsScreen({
-    this.genre,
-  });
-
-  final String genre;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(genre),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            ElevatedButton(
-              onPressed: () => Beamer.of(context).updateCurrentLocation(
-                pathBlueprint: '/books',
-                rewriteParameters: true,
-              ),
-              child: Text('Go back to books'),
-            ),
-            ElevatedButton(
-              onPressed: () => Beamer.of(context)
-                  .beamTo(ArticlesLocation(pathBlueprint: '/articles')),
-              child: Text('Beam to articles'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class ArticlesScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Articles'),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 8.0),
-            child: GestureDetector(
-              onTap: () => Beamer.of(context).beamBack(),
-              child: Row(
-                children: [Text('Beam back  '), Icon(Icons.backup)],
-              ),
-            ),
-          ),
-        ],
-      ),
-      body: ListView(
-        children: articles
-            .map((article) => ListTile(
-                  title: Text(article['title']),
-                  subtitle: Text(article['author']),
-                  onTap: () => Beamer.of(context).updateCurrentLocation(
-                    pathBlueprint: '/articles/:articleId',
-                    pathParameters: {'articleId': article['id']},
-                  ),
-                ))
-            .toList(),
-      ),
-    );
-  }
-}
-
-class ArticleDetailsScreen extends StatelessWidget {
-  ArticleDetailsScreen({
-    this.articleId,
-  }) : article = articles.firstWhere((article) => article['id'] == articleId);
-
-  final String articleId;
-  final Map<String, String> article;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(article['title']),
-      ),
-      body: Text('Author: ${article['author']}'),
     );
   }
 }
@@ -307,20 +108,13 @@ class BooksLocation extends BeamLocation {
   BooksLocation({
     String pathBlueprint,
     Map<String, String> pathParameters,
-    Map<String, String> queryParameters,
-    Map<String, dynamic> data,
   }) : super(
           pathBlueprint: pathBlueprint,
           pathParameters: pathParameters,
-          queryParameters: queryParameters,
-          data: data,
         );
 
   @override
-  List<String> get pathBlueprints => [
-        '/books/:bookId/genres/:genreId',
-        '/books/:bookId/buy',
-      ];
+  List<String> get pathBlueprints => ['/books/:bookId'];
 
   @override
   List<BeamPage> get pages => [
@@ -339,61 +133,6 @@ class BooksLocation extends BeamLocation {
               bookId: pathParameters['bookId'],
             ),
           ),
-        if (pathSegments.contains('buy'))
-          BeamPage(
-            key: ValueKey('book-${pathParameters['bookId']}-buy'),
-            child: BuyScreen(
-              book: data['book'],
-            ),
-          ),
-        if (pathSegments.contains('genres'))
-          BeamPage(
-            key: ValueKey('book-${pathParameters['bookId']}-genres'),
-            child: GenresScreen(
-              book: data['book'],
-            ),
-          ),
-        if (pathParameters.containsKey('genreId'))
-          BeamPage(
-            key: ValueKey('genres-${pathParameters['genreId']}'),
-            child: GenreDetailsScreen(
-              genre: data['genre'],
-            ),
-          ),
-      ];
-}
-
-class ArticlesLocation extends BeamLocation {
-  ArticlesLocation({
-    String pathBlueprint,
-    Map<String, String> pathParameters,
-    Map<String, String> queryParameters,
-    Map<String, dynamic> data,
-  }) : super(
-          pathBlueprint: pathBlueprint,
-          pathParameters: pathParameters,
-          queryParameters: queryParameters,
-          data: data,
-        );
-
-  @override
-  List<String> get pathBlueprints => ['/articles/:articleId'];
-
-  @override
-  List<BeamPage> get pages => [
-        ...HomeLocation().pages,
-        if (pathSegments.contains('articles'))
-          BeamPage(
-            key: ValueKey('articles'),
-            child: ArticlesScreen(),
-          ),
-        if (pathParameters.containsKey('articleId'))
-          BeamPage(
-            key: ValueKey('articles-${pathParameters['articleId']}'),
-            child: ArticleDetailsScreen(
-              articleId: pathParameters['articleId'],
-            ),
-          ),
       ];
 }
 
@@ -403,15 +142,13 @@ class MyApp extends StatelessWidget {
   final List<BeamLocation> beamLocations = [
     HomeLocation(),
     BooksLocation(),
-    ArticlesLocation(),
   ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
       routerDelegate: BeamerRouterDelegate(
         initialLocation: initialLocation,
-        notFoundPage: Scaffold(body: Center(child: Text('Not found'))),
       ),
       routeInformationParser: BeamerRouteInformationParser(
         beamLocations: beamLocations,
