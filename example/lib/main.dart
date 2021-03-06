f5f6e52ea2624a6cbacae604def12a0353d40ea6
youssefali424 <youssef4242014@gmail.com>
2021-03-06 15:43:12 +0200

    [fix] example to match last release

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -133,11 +133,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var routerDelegate = BeamerRouterDelegate(
+      beamLocations: beamLocations,
+    );
     return MaterialApp.router(
-      routerDelegate: BeamerRouterDelegate(
-        beamLocations: beamLocations,
-      ),
+      routerDelegate: routerDelegate,
       routeInformationParser: BeamerRouteInformationParser(),
+      backButtonDispatcher:
+          BeamerBackButtonDispatcher(delegate: routerDelegate),
     );
   }
 }
