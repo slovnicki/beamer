c4912dd35358be8f729b64cd544f943d1ae93d81
Sandro Lovnički <lovnicki.sandro@gmail.com>
2021-03-31 01:42:02 +0200

    {beam_page}: [add] BeamPageType and custom pageRouteBuilder
    	- closes https://github.com/slovnicki/beamer/issues/141

--- package/lib/src/beam_page.dart
+++ package/lib/src/beam_page.dart
@@ -1,4 +1,13 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+/// Types for how to route should be built.
+enum BeamPageType {
+  material,
+  cupertino,
+  noTransition,
+  fadeTransition,
+}
 
 /// A wrapper for pages / screens that will be drawn.
 class BeamPage extends Page {
@@ -6,11 +15,24 @@ class BeamPage extends Page {
     Key key,
     String name,
     @required this.child,
+    this.type = BeamPageType.material,
+    this.pageRouteBuilder,
     this.keepQueryOnPop = false,
   }) : super(key: key, name: name);
 
   /// The concrete Widget representing app's screen.
   final Widget child;
+
+  /// The type to determine how a route should be built.
+  ///
+  /// See [BeamPageType] for available types.
+  final BeamPageType type;
+
+  /// A completely custom [PageRouteBuilder] to use for [createRoute].
+  ///
+  /// `settings` must be passed to [PageRouteBuilder.settings].
+  final PageRouteBuilder Function(RouteSettings settings, Widget child)
+      pageRouteBuilder;
 
   /// When this [BeamPage] pops from [Navigator] stack, whether to keep the
   /// query parameters within current [BeamLocation].
@@ -20,10 +42,40 @@ class BeamPage extends Page {
 
   @override
   Route createRoute(BuildContext context) {
-    return MaterialPageRoute(
-      settings: this,
-      builder: (context) => child,
-    );
+    if (pageRouteBuilder != null) {
+      return pageRouteBuilder(this, child);
+    }
+    switch (type) {
+      case BeamPageType.material:
+        return MaterialPageRoute(
+          settings: this,
+          builder: (context) => child,
+        );
+      case BeamPageType.cupertino:
+        return CupertinoPageRoute(
+          settings: this,
+          builder: (context) => child,
+        );
+      case BeamPageType.noTransition:
+        return PageRouteBuilder(
+          settings: this,
+          pageBuilder: (context, animation, secondaryAnimation) => child,
+        );
+      case BeamPageType.fadeTransition:
+        return PageRouteBuilder(
+          settings: this,
+          pageBuilder: (_, __, ___) => child,
+          transitionsBuilder: (_, animation, __, child) => FadeTransition(
+            opacity: animation,
+            child: child,
+          ),
+        );
+      default:
+        return MaterialPageRoute(
+          settings: this,
+          builder: (context) => child,
+        );
+    }
   }
 
   @override
