cf140614f1b6e1077b05c7fbaf399625062d3846
Sandro Lovnički <lovnicki.sandro@gmail.com>
2021-04-22 01:30:12 +0200

    {beam_location}: [add] canHandle(), [move] SimpleBeamLocation

--- package/lib/src/beam_location.dart
+++ package/lib/src/beam_location.dart
@@ -1,5 +1,6 @@
 import 'package:beamer/beamer.dart';
 import 'package:beamer/src/beam_state.dart';
+import 'package:beamer/src/utils.dart';
 import 'package:flutter/widgets.dart';
 
 /// Configuration for a navigatable application region.
@@ -37,6 +38,13 @@ abstract class BeamLocation<T extends BeamState> extends ChangeNotifier {
       notifyListeners();
     }
   }
+
+  /// Can this handle the `uri` based on its [pathBlueprints].
+  ///
+  /// Can be useful in a custom [BeamerRouterDelegate.locationBuilder].
+  ///
+  /// Used in [BeamerRouterDelegate._updateFromParent].
+  bool canHandle(Uri uri) => Utils.canBeamLocationHandleUri(this, uri);
 
   /// Gives the ability to wrap the `navigator`.
   ///
@@ -103,4 +111,84 @@ class NotFound extends BeamLocation {
 
   @override
   List<String> get pathBlueprints => [''];
+}
+
+/// A beam location for [SimpleLocationBuilder], but can be used freely.
+///
+/// Useful when needing a simple beam location with a single or few pages.
+class SimpleBeamLocation extends BeamLocation {
+  SimpleBeamLocation({
+    required BeamState state,
+    required this.routes,
+    this.navBuilder,
+  }) : super(state);
+
+  /// Map of all routes this location handles.
+  Map<String, WidgetBuilder> routes;
+
+  /// A wrapper used as [BeamLocation.builder].
+  Widget Function(BuildContext context, Widget navigator)? navBuilder;
+
+  @override
+  Widget builder(BuildContext context, Widget navigator) {
+    return navBuilder?.call(context, navigator) ?? navigator;
+  }
+
+  List<String> get sortedRoutes =>
+      routes.keys.toList()..sort((a, b) => a.length - b.length);
+
+  @override
+  List<String> get pathBlueprints => routes.keys.toList();
+
+  @override
+  List<BeamPage> pagesBuilder(BuildContext context, BeamState state) {
+    var filteredRoutes = chooseRoutes(state, routes.keys);
+    final activeRoutes = Map.from(routes)
+      ..removeWhere((key, value) => !filteredRoutes.containsKey(key));
+    final sortedRoutes = activeRoutes.keys.toList()
+      ..sort((a, b) => a.length - b.length);
+    return sortedRoutes.map((route) {
+      return BeamPage(
+        key: ValueKey(filteredRoutes[route]),
+        child: routes[route]!(context),
+      );
+    }).toList();
+  }
+
+  static Map<String, String> chooseRoutes(
+      BeamState state, Iterable<String> routes) {
+    var matched = <String, String>{};
+    for (var route in routes) {
+      final uriPathSegments = List.from(state.uri.pathSegments);
+      if (uriPathSegments.length > 1 && uriPathSegments.last == '') {
+        uriPathSegments.removeLast();
+      }
+
+      final routePathSegments = Uri.parse(route).pathSegments;
+
+      if (uriPathSegments.length < routePathSegments.length) {
+        continue;
+      }
+
+      var checksPassed = true;
+      var path = '';
+
+      for (int i = 0; i < routePathSegments.length; i++) {
+        path += '/${uriPathSegments[i]}';
+        if (routePathSegments[i] == '*' ||
+            routePathSegments[i].startsWith(':')) {
+          continue;
+        }
+        if (routePathSegments[i] != uriPathSegments[i]) {
+          checksPassed = false;
+          break;
+        }
+      }
+
+      if (checksPassed) {
+        matched[route] = path;
+      }
+    }
+    return matched;
+  }
 }
--- package/lib/src/beamer_location_builder.dart
+++ package/lib/src/beamer_location_builder.dart
@@ -22,89 +22,20 @@ class SimpleLocationBuilder implements Function {
   /// List of all routes this builder handles.
   final Map<String, WidgetBuilder> routes;
 
+  /// Used as a [BeamLocation.builder].
   Widget Function(BuildContext context, Widget navigator)? builder;
 
   BeamLocation call(BeamState state) {
     var matched = SimpleBeamLocation.chooseRoutes(state, routes.keys);
     if (matched.isNotEmpty) {
       return SimpleBeamLocation(
-        state,
-        Map.fromEntries(
+        state: state,
+        routes: Map.fromEntries(
             routes.entries.where((e) => matched.containsKey(e.key))),
-        builder,
+        navBuilder: builder,
       );
     } else {
       return NotFound(path: state.uri.path);
     }
-  }
-}
-
-class SimpleBeamLocation extends BeamLocation {
-  SimpleBeamLocation(BeamState state, this.routes, this.navBuilder)
-      : super(state);
-
-  /// Map of all routes this location handles.
-  Map<String, WidgetBuilder> routes;
-
-  Widget Function(BuildContext context, Widget navigator)? navBuilder;
-
-  @override
-  Widget builder(BuildContext context, Widget navigator) {
-    return navBuilder?.call(context, navigator) ?? navigator;
-  }
-
-  List<String> get sortedRoutes =>
-      routes.keys.toList()..sort((a, b) => a.length - b.length);
-
-  @override
-  List<String> get pathBlueprints => [sortedRoutes.last];
-
-  @override
-  List<BeamPage> pagesBuilder(BuildContext context, BeamState state) {
-    var filteredRoutes = chooseRoutes(state, routes.keys);
-    routes.removeWhere((key, value) => !filteredRoutes.containsKey(key));
-    return sortedRoutes.map((route) {
-      return BeamPage(
-        key: ValueKey(filteredRoutes[route]),
-        child: routes[route]!(context),
-      );
-    }).toList();
-  }
-
-  static Map<String, String> chooseRoutes(
-      BeamState state, Iterable<String> routes) {
-    var matched = <String, String>{};
-    for (var route in routes) {
-      final uriPathSegments = List.from(state.uri.pathSegments);
-      if (uriPathSegments.length > 1 && uriPathSegments.last == '') {
-        uriPathSegments.removeLast();
-      }
-
-      final routePathSegments = Uri.parse(route).pathSegments;
-
-      if (uriPathSegments.length < routePathSegments.length) {
-        continue;
-      }
-
-      var checksPassed = true;
-      var path = '';
-
-      for (int i = 0; i < routePathSegments.length; i++) {
-        path += '/${uriPathSegments[i]}';
-        if (routePathSegments[i] == '*' ||
-            routePathSegments[i].startsWith(':')) {
-          continue;
-        }
-        if (routePathSegments[i] != uriPathSegments[i]) {
-          checksPassed = false;
-          break;
-        }
-      }
-
-      if (checksPassed) {
-        matched[route] = path;
-      }
-    }
-    return matched;
   }
 }
