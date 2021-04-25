450dda8d87e1be26734689b128af678bd9d750db
Sandro Lovnički <lovnicki.sandro@gmail.com>
2021-04-25 13:20:28 +0200

    test/: [add] SimpleBeamLocation test group

--- package/test/beam_location_test.dart
+++ package/test/beam_location_test.dart
@@ -1,4 +1,6 @@
-import 'package:beamer/src/beam_state.dart';
+import 'package:flutter/material.dart';
+import 'package:beamer/beamer.dart';
+
 import 'package:flutter_test/flutter_test.dart';
 
 import 'test_locations.dart';
@@ -12,6 +14,35 @@ void main() {
         queryParameters: {'q': 'xxx'},
       );
       expect(location2.state.uri.toString(), '/l2/42?q=xxx');
+    });
+  });
+
+  group('SimpleBeamLocation', () {
+    final delegate = BeamerRouterDelegate(
+      locationBuilder: SimpleLocationBuilder(
+        routes: {
+          '/': (context) => Container(),
+        },
+      ),
+    );
+    delegate.setNewRoutePath(Uri.parse('/'));
+
+    test('SimpleBeamLocation takes query', () {
+      expect(delegate.currentLocation.state.queryParameters, equals({}));
+      delegate.beamToNamed('/?q=t');
+      expect(
+          delegate.currentLocation.state.queryParameters, equals({'q': 't'}));
+    });
+
+    testWidgets('SimpleBeamLocation includes query in page key',
+        (tester) async {
+      await tester.pumpWidget(MaterialApp.router(
+        routeInformationParser: BeamerRouteInformationParser(),
+        routerDelegate: delegate,
+      ));
+      expect(delegate.currentPages.last.key, isA<ValueKey>());
+      expect((delegate.currentPages.last.key as ValueKey).value,
+          equals(ValueKey('/?q=t').value));
     });
   });
 }
