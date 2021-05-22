de2b09acb28ab014e2d3adfc20a70e7ed2ba791f
HatEmU <abusettahatem@gmail.com>
2021-05-22 13:59:34 +0200

    Use RegExp inside utility methods.

--- package/lib/src/utils.dart
+++ package/lib/src/utils.dart
@@ -34,33 +34,38 @@ abstract class Utils {
   /// Used in [BeamLocation.canHandle] and [chooseBeamLocation].
   static bool canBeamLocationHandleUri(BeamLocation beamLocation, Uri uri) {
     for (var pathBlueprint in beamLocation.pathBlueprints) {
-      if (pathBlueprint == uri.path || pathBlueprint == '/*') {
-        return true;
-      }
-      final uriPathSegments = List.from(uri.pathSegments);
-      if (uriPathSegments.length > 1 && uriPathSegments.last == '') {
-        uriPathSegments.removeLast();
-      }
-      final beamLocationPathBlueprintSegments =
-          Uri.parse(pathBlueprint).pathSegments;
-      if (uriPathSegments.length > beamLocationPathBlueprintSegments.length &&
-          !beamLocationPathBlueprintSegments.contains('*')) {
-        continue;
-      }
-      var checksPassed = true;
-      for (int i = 0; i < uriPathSegments.length; i++) {
-        if (beamLocationPathBlueprintSegments[i] == '*') {
-          checksPassed = true;
-          break;
+      if (pathBlueprint is String) {
+        if (pathBlueprint == uri.path || pathBlueprint == '/*') {
+          return true;
         }
-        if (uriPathSegments[i] != beamLocationPathBlueprintSegments[i] &&
-            beamLocationPathBlueprintSegments[i][0] != ':') {
-          checksPassed = false;
-          break;
+        final uriPathSegments = List.from(uri.pathSegments);
+        if (uriPathSegments.length > 1 && uriPathSegments.last == '') {
+          uriPathSegments.removeLast();
         }
-      }
-      if (checksPassed) {
-        return true;
+        final beamLocationPathBlueprintSegments =
+            Uri.parse(pathBlueprint).pathSegments;
+        if (uriPathSegments.length > beamLocationPathBlueprintSegments.length &&
+            !beamLocationPathBlueprintSegments.contains('*')) {
+          continue;
+        }
+        var checksPassed = true;
+        for (int i = 0; i < uriPathSegments.length; i++) {
+          if (beamLocationPathBlueprintSegments[i] == '*') {
+            checksPassed = true;
+            break;
+          }
+          if (uriPathSegments[i] != beamLocationPathBlueprintSegments[i] &&
+              beamLocationPathBlueprintSegments[i][0] != ':') {
+            checksPassed = false;
+            break;
+          }
+        }
+        if (checksPassed) {
+          return true;
+        }
+      } else {
+        final regexp = pathBlueprint as RegExp;
+        return regexp.hasMatch(uri.toString());
       }
     }
     return false;
@@ -75,52 +80,76 @@ abstract class Utils {
     Map<String, dynamic> data = const <String, dynamic>{},
   }) {
     if (beamLocation != null) {
+      // TODO: abstract this and reuse in canBeamLocationHandleUri
       for (var pathBlueprint in beamLocation.pathBlueprints) {
-        if (pathBlueprint == uri.path || pathBlueprint == '/*') {
-          BeamState(
-            pathBlueprintSegments: uri.pathSegments,
-            queryParameters: uri.queryParameters,
-            data: data,
-          );
-        }
-        final uriPathSegments = List.from(uri.pathSegments);
-        if (uriPathSegments.length > 1 && uriPathSegments.last == '') {
-          uriPathSegments.removeLast();
-        }
-        final beamLocationPathBlueprintSegments =
-            Uri.parse(pathBlueprint).pathSegments;
-        var pathSegments = <String>[];
-        var pathParameters = <String, String>{};
-        if (uriPathSegments.length > beamLocationPathBlueprintSegments.length &&
-            !beamLocationPathBlueprintSegments.contains('*')) {
-          continue;
-        }
-        var checksPassed = true;
-        for (int i = 0; i < uriPathSegments.length; i++) {
-          if (beamLocationPathBlueprintSegments[i] == '*') {
-            pathSegments = List<String>.from(uriPathSegments);
-            checksPassed = true;
-            break;
+        if (pathBlueprint is String) {
+          if (pathBlueprint == uri.path || pathBlueprint == '/*') {
+            BeamState(
+              pathBlueprintSegments: uri.pathSegments,
+              queryParameters: uri.queryParameters,
+              data: data,
+            );
           }
-          if (uriPathSegments[i] != beamLocationPathBlueprintSegments[i] &&
-              beamLocationPathBlueprintSegments[i][0] != ':') {
-            checksPassed = false;
-            break;
-          } else if (beamLocationPathBlueprintSegments[i][0] == ':') {
-            pathParameters[beamLocationPathBlueprintSegments[i].substring(1)] =
-                uriPathSegments[i];
-            pathSegments.add(beamLocationPathBlueprintSegments[i]);
-          } else {
-            pathSegments.add(uriPathSegments[i]);
+          final uriPathSegments = List.from(uri.pathSegments);
+          if (uriPathSegments.length > 1 && uriPathSegments.last == '') {
+            uriPathSegments.removeLast();
           }
-        }
-        if (checksPassed) {
-          return BeamState(
-            pathBlueprintSegments: pathSegments,
-            pathParameters: pathParameters,
-            queryParameters: uri.queryParameters,
-            data: data,
-          );
+          final beamLocationPathBlueprintSegments =
+              Uri.parse(pathBlueprint).pathSegments;
+          var pathSegments = <String>[];
+          var pathParameters = <String, String>{};
+          if (uriPathSegments.length >
+                  beamLocationPathBlueprintSegments.length &&
+              !beamLocationPathBlueprintSegments.contains('*')) {
+            continue;
+          }
+          var checksPassed = true;
+          for (int i = 0; i < uriPathSegments.length; i++) {
+            if (beamLocationPathBlueprintSegments[i] == '*') {
+              pathSegments = List<String>.from(uriPathSegments);
+              checksPassed = true;
+              break;
+            }
+            if (uriPathSegments[i] != beamLocationPathBlueprintSegments[i] &&
+                beamLocationPathBlueprintSegments[i][0] != ':') {
+              checksPassed = false;
+              break;
+            } else if (beamLocationPathBlueprintSegments[i][0] == ':') {
+              pathParameters[beamLocationPathBlueprintSegments[i]
+                  .substring(1)] = uriPathSegments[i];
+              pathSegments.add(beamLocationPathBlueprintSegments[i]);
+            } else {
+              pathSegments.add(uriPathSegments[i]);
+            }
+          }
+          if (checksPassed) {
+            return BeamState(
+              pathBlueprintSegments: pathSegments,
+              pathParameters: pathParameters,
+              queryParameters: uri.queryParameters,
+              data: data,
+            );
+          }
+        } else {
+          final regexp = pathBlueprint as RegExp;
+          final beamLocationPathBlueprintSegments =
+              Uri.parse(pathBlueprint.toString()).pathSegments;
+
+          var pathParameters = <String, String>{};
+          for (int i = 0; i < uri.pathSegments.length; i++) {
+            if (beamLocationPathBlueprintSegments[i][0] == ':') {
+              pathParameters[beamLocationPathBlueprintSegments[i]
+                  .substring(1)] = uri.pathSegments[i];
+            }
+          }
+          if (regexp.hasMatch(uri.toString())) {
+            return BeamState(
+              pathBlueprintSegments: uri.pathSegments,
+              pathParameters: pathParameters,
+              queryParameters: uri.queryParameters,
+              data: data,
+            );
+          }
         }
       }
     }
@@ -131,20 +160,26 @@ abstract class Utils {
     );
   }
 
-  static bool urisMatch(Uri blueprint, Uri exact) {
-    final blueprintSegments = blueprint.pathSegments;
-    final exactSegment = exact.pathSegments;
-    if (blueprintSegments.length != exactSegment.length) {
-      return false;
-    }
-    for (int i = 0; i < blueprintSegments.length; i++) {
-      if (blueprintSegments[i].startsWith(':')) {
-        continue;
-      }
-      if (blueprintSegments[i] != exactSegment[i]) {
+  static bool urisMatch(dynamic blueprint, Uri exact) {
+    if (blueprint is String) {
+      blueprint = Uri.parse(blueprint);
+      final blueprintSegments = blueprint.pathSegments;
+      final exactSegment = exact.pathSegments;
+      if (blueprintSegments.length != exactSegment.length) {
         return false;
       }
+      for (int i = 0; i < blueprintSegments.length; i++) {
+        if (blueprintSegments[i].startsWith(':')) {
+          continue;
+        }
+        if (blueprintSegments[i] != exactSegment[i]) {
+          return false;
+        }
+      }
+      return true;
+    } else {
+      blueprint = blueprint as RegExp;
+      return blueprint.hasMatch(exact.toString());
     }
-    return true;
   }
 }
