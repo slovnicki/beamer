839256591ddaf5ea20c76dc285fd5883f4086154
Guilherme C. Matuella <guilherme1matuella@gmail.com>
2021-12-07 16:58:37 -0300

    Fix warnings in `examples/authentication_riverpod`

--- examples/authentication_riverpod/lib/views/home_page.dart
+++ examples/authentication_riverpod/lib/views/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:authentication_riverpod/providers/provider.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class HomePage extends HookConsumerWidget {
